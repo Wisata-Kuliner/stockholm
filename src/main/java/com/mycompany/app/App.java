f2b7b083b6aa84c2ca0702db8530201ab6704ba8
Bryanza Novirahman <bryanzanr@gmail.com>
2018-03-30 14:42:23 +0700

    Prepare for Input & Output Program

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,13 +1,32 @@
 package com.mycompany.app;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
 
 /**
- * Hello world!
+ * Main Application Class
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+	/**
+	* Main Method
+	*/
+    public static void main( String[] args ) {
+        try{
+        	// Initialize Input & Output
+        	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        	
+        	bw.write( "Hello World!\n" );
+        	// Print Output to Console
+        	bw.flush();
+
+        }catch(IOException ie){
+        	ie.printStackTrace();
+        }
+        // End Program
+        System.exit(1);
     }
 }
